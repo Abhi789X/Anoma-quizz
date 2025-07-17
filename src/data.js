4b33bb466154bbf6eb123638d0231f6e17213762
Abhi789X <87174484+Abhi789X@users.noreply.github.com>
2025-07-17 16:15:36 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const questions = [
+  {
+    question: "What is Anoma’s core innovation?",
+    options: ["zkVM", "Intent-centric architecture", "Rollups", "Data availability"],
+    answer: "Intent-centric architecture",
+  },
+  {
+    question: "What consensus mechanism does Anoma use?",
+    options: ["PoS", "Proof of Intents", "Proof of Work", "Nakamoto Consensus"],
+    answer: "Proof of Intents",
+  },
+  {
+    question: "What is 'intent gossip' in Anoma?",
+    options: ["Spam protection", "Computation layer", "Intent discovery", "Fee payment"],
+    answer: "Intent discovery",
+  },
+  {
+    question: "What's the name of Anoma’s zk component?",
+    options: ["Taiga", "Nomos", "Aztec", "Halo2"],
+    answer: "Taiga",
+  },
+  {
+    question: "Which language is used for writing intents?",
+    options: ["Solidity", "Rust", "IntentScript", "Juvix"],
+    answer: "Juvix",
+  },
+  {
+    question: "What layer is Anoma?",
+    options: ["L1", "L2", "L3", "L0"],
+    answer: "L1",
+  },
+  {
+    question: "What's Namada?",
+    options: ["Stablecoin", "Privacy L1 by Anoma", "Wallet", "Rollup"],
+    answer: "Privacy L1 by Anoma",
+  },
+  {
+    question: "What are intents matched by?",
+    options: ["Builders", "Rollers", "Solvers", "Validators"],
+    answer: "Solvers",
+  }
+];
